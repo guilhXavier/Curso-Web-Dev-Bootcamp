f960c42300555ddf6443e985a6e56569d532fcca
guilhXavier <guilhermetamara5@gmail.com>
2019-01-31 19:03:39 -0200

    ejs in section 26

--- Unit 26/app.js
+++ Unit 26/app.js
@@ -1,14 +1,27 @@
 var Express = require("express");
 var app = Express();
 
-app.get("/", function(req, res){
-  res.render("home.ejs");
+app.use(Express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("home");
 });
 
 // fallInLoveWith/rusty
 app.get("/fallInLoveWith/:thing", function(req, res) {
   let thing = req.params.thing;
-  res.send("You fell in love with " + thing);
+  res.render("love", { thingLet: thing });
+});
+
+app.get("/posts", function(req, res) {
+  let posts = [
+    { title: "Post 1", author: "guilh" },
+    { title: "Post 2", author: "gui" },
+    { title: "Post 3", author: "g" }
+  ];
+
+  res.render("posts.ejs", { posts: posts });
 });
 
 app.listen(8080, function () {
