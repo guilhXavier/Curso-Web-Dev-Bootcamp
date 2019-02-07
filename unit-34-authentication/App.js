3ed586f1c2f0fd98d26a7ce99eb52b557226144b
guilhXavier <guilhermetamara5@gmail.com>
2019-02-07 11:12:50 -0200

    sign up and login

--- unit-34-authentication/App.js
+++ unit-34-authentication/App.js
@@ -2,14 +2,30 @@ let express               = require('express'),
     mongoose              = require('mongoose'),
     passport              = require('passport'),
     bodyParser            = require('body-parser'),
+    User                  = require('./models/user'),
     localStrategy         = require('passport-local'),
     passportLocalMongoose = require('passport-local-mongoose');
 
 mongoose.connect('mongodb://localhost:8080/auth-demo-app');
+
 let app = express();
 
+app.use(require('express-session')({
+    secret: 'Rusty is the best and cutest dog in the world',
+    resave: false,
+    saveUninitialized: false
+}));
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//ROUTES
 app.get("/", function(req, res) {
     res.render('home');
 });
@@ -17,6 +33,40 @@ app.get("/", function(req, res) {
 app.get("/secret", function(req, res) {
     res.render("secret");
 });
+
+// AUTH ROUTES
+
+// show signup form
+app.get('/register', function(req, res) {
+    res.render("register");
+});
+
+// handling user sign up
+app.post("/register", function(req, res) {
+    User.register(new User({ username: req.body.username }), req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect('/secret');
+        });
+    });
+});
+
+// LOGIN ROUTES
+// render login form
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+// login logic middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: '/secret',
+    failureRedirect: '/login'
+}, function(req, res) {
+
+}));
 
 app.listen(5000, function() {
     console.log("WE'RE LIVE");
