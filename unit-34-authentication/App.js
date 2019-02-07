39e5ea44c45831d44c2f4d76bb1c1ecd2295b8af
guilhXavier <guilhermetamara5@gmail.com>
2019-02-07 11:41:51 -0200

    finishing auth

--- unit-34-authentication/App.js
+++ unit-34-authentication/App.js
@@ -30,7 +30,7 @@ app.get("/", function(req, res) {
     res.render('home');
 });
 
-app.get("/secret", function(req, res) {
+app.get("/secret", isLoggedIn, function(req, res) {
     res.render("secret");
 });
 
@@ -61,12 +61,24 @@ app.get('/login', function(req, res) {
 });
 
 // login logic middleware
-app.post("/login", passport.authenticate("local", {
+app.post("/login", passport.authenticate('local', {
     successRedirect: '/secret',
     failureRedirect: '/login'
-}, function(req, res) {
+}), function(req, res) {
+});
 
-}));
+// LOGOUT ROUTES
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 app.listen(5000, function() {
     console.log("WE'RE LIVE");
